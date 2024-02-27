d96b7a81605605e9ae9c5da9b9a23242f284145c
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-02-27 15:28:59 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const mainRouter = require("./routes/index");
+const colors = require("colors");
+require("dotenv").config({ path: "../.env" }); // Adjust the path as needed
 
 const app = express();
-app.use("/api/v1/", mainRouter);
+
+app.use("/api/v1", mainRouter);
+
+app.get("/", function (req, res) {
+  res.send("hi");
+});
+
+app.listen(process.env.PORT, function () {
+  console.log(`Port is running on ${process.env.PORT}`);
+});
