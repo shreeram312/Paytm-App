32caefde9fa045d5846ea75ea42539647682191c
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-03-05 00:15:38 +0530

    Filtering the Usrrs

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -120,4 +120,21 @@ router.put("/", authMiddleware, async (req, res) => {
   });
 });
 
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [{ firstName: { $regex: filter } }, { lastName: { $regex: filter } }],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      userName: user.userName,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
 module.exports = router;
